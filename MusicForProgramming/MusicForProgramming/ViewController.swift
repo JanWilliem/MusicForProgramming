389e8f70a7dd8ac469c4a61fe2ea17f6fac9155e
wanghaidong <smithw1989@icloud.com>
2016-01-05 00:17:10 +0800

    2 commit

---
+++ MusicForProgramming/MusicForProgramming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicForProgramming
+//
+//  Created by Smithw on 16/1/5.
+//  Copyright © 2016年 Smithw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
